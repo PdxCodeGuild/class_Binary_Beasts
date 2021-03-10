f06bb06993324d309060c988d7a2540289066992
Sean <seanwilcox@comcast.net>
2021-03-09 20:24:53 -0800

    Lab04

---
+++ Students/Sean/python/Lab04.py
@@ -0,0 +1,24 @@
+import random
+
+predictions = [
+    'Pretty sure, yeah.',
+    'For sure, yes.',
+    'Not gonna happen.',
+    'Try again later.',
+    'Don\'t hold your breath.',
+    'Your guess is as good as mine.',
+]
+
+print('Welcome to the magic 8 ball!\nAll your questions can soon be answered.')
+
+while True:
+
+    answer = random.choice(predictions)
+    user_question = input('Ask a question, or enter "done" if you don\'t belive in magic.\n>')
+
+    if user_question[-1] != '?':
+        print('Please ask a valid question.')
+    elif user_question == 'done':
+        break
+    else:
+        print(f'I\'m asking the ball "{user_question}"\nI\'m getting a reply of "{answer}"\n')

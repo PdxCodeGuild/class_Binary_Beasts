f201f75be2b1707846a8089c60d4778ab3219f89
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-09 20:38:17 -0800

    Added space for readability

--- Students/Daniel/Python/lab05.py
+++ Students/Daniel/Python/lab05.py
@@ -5,6 +5,8 @@ list_of_eyes = ["^","-","*"]
 list_of_mouths = ["__", "_", "."]
 generated_emoticon = ""
 loop_count = 0
+
+
 while loop_count != 5:
     generated_emoticon = random.choice(list_of_eyes)
     generated_emoticon += random.choice(list_of_mouths)

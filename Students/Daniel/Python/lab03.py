30ea1bd8ab7a7ac48f55e461d702e9fb52aee18b
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-09 19:35:19 -0800

    Pushing refactored version

--- Students/Daniel/Python/lab03.py
+++ Students/Daniel/Python/lab03.py
@@ -1,13 +1,22 @@
 user_input = input("Please enter a number representing a letter grade of (0-100): ")
 user_input = int(user_input)
-if user_input >= 90:
-    print("A")
-elif user_input >= 80:
-    print("B")
-elif user_input >= 70:
-    print("C")
-elif user_input >= 60:
-    print("D")
+user_grade = ""
+user_sign = ""
+if user_input % 10 >= 5:
+    user_sign = "+"
 else:
-    print("F")
-print(bool(1))
+    user_sign = "-"
+
+if user_input >= 90:
+    user_grade = "A"
+elif user_input >= 80:
+    user_grade = "B"
+elif user_input >= 70:
+    user_grade = "C"
+elif user_input >= 60:
+    user_grade = "D"
+else:
+    user_grade = "F"
+
+print(user_grade + user_sign)
+

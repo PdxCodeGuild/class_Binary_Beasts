b5f1f7633bc1fec6293492b0116ad1b2220cf5cf
Bobby <bobbyestes68@gmail.com>
2021-03-10 20:21:47 -0800

    loading lab #5

---
+++ Students/bobby/python/lab_5.py
@@ -0,0 +1,14 @@
+import random
+
+eyes = [";", ":"]
+nose = ["<", ">"]
+mouth = [")", "(", "@"]
+ran_eyes = random.choice(eyes)
+ran_nose = random.choice(nose)
+ran_mouth = random.choice(mouth)
+
+
+print (ran_eyes)
+print (ran_nose)
+print (ran_mouth)
+print (ran_eyes,ran_nose,ran_mouth)

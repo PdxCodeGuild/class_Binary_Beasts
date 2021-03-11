00a0e73aa6af033ff30e09955e934eabc6935db1
Theo Cocco <theo@Theos-MacBook-Air.local>
2021-03-10 21:13:58 -0800

    lab 8 version 4

--- Students/Theo/Python/lab8.py
+++ Students/Theo/Python/lab8.py
@@ -1,6 +1,11 @@
 from random import randint as random
+
 computer = random(1,10)
+
 count = 0
+
+last_guess = 0
+
 while True:
     user = input('Pick a number between 1 and 10: ')
     
@@ -12,8 +17,20 @@ while True:
         print(f'Correct! You guessed {count} times.')
         break
     
+    if count > 1:
+        if abs(user - computer) > abs(last_guess - computer):
+            print('Colder')
+        elif abs(user - computer) < abs(last_guess - computer):
+            print('Warmer')
+            
     elif user > computer:
         print('Too high!')
+
     
     elif user < computer:
         print('Too low!')
+    
+    last_guess = user
+
+
+

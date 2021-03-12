5a520b599bab25a552086d9ac4a680801fe7c512
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-11 20:59:28 -0800

    Added lab 10

--- Students/Daniel/Python/lab10.py
+++ Students/Daniel/Python/lab10.py
@@ -1,16 +1,18 @@
 def peaks(data):
-    print(data)
-    y = 0
-    for x in data:
-        if data[y-1]< x > data[y+1]:
-            print(str(x) + "Added")
+    for x in range(len(test_data)-1):
+        if test_data[x -1] < test_data[x] > test_data[x+1]:
             peaks_list.append(x)
-        y+=1
-def valleys(data):
-    return 0
+            peaks_and_valleys.append(x)
 
-def peaks_and_valleys(data):
-    return 0
+
+
+def valleys(data):
+    for x in range(len(test_data)-1):
+        x+=1
+        if test_data[x -1] > test_data[x] < test_data[x+1]:
+            valleys_list.append(x)
+            peaks_and_valleys.append(x)
+    
 
 test_data = [1,2,3,4,5,6,7,6,5,4,5,6,7,8,9,8,7,6,7,8,9]
 
@@ -22,5 +24,10 @@ valleys_list = []
 
 peaks_and_valleys = []
 
+
 peaks(test_data)
 print(peaks_list)
+valleys(test_data)
+print(valleys_list)
+peaks_and_valleys = sorted(peaks_and_valleys)
+print(peaks_and_valleys)

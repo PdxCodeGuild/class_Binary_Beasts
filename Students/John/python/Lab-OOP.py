dbed34a0e877b1d926c36bc1c4f29a886c57fcf5
jrobson1065 <j.robson1065@gmail.com>
2021-03-16 10:21:14 -0400

    Added objects to OOP lab

--- Students/John/python/Lab-OOP.py
+++ Students/John/python/Lab-OOP.py
@@ -16,7 +16,14 @@ class Bank_Account:
         self.balance *= 0.95
         
     def display(self):
-        print(self.balance)
+        print(f"Account Number: {self.account_number}\nName: {self.name}\nBalance: ${self.balance}")
+        
+print("Bank Account")
+b = Bank_Account(1234, "Kyle", 2000)
+b.deposit(700)
+b.withdraw(200)
+b.bank_fees()
+b.display()
 
 
 #Rectangle
@@ -33,7 +40,7 @@ class Rectangle:
         self.area = self.length * self.width
         
     def display(self):
-        print(f"Length: {self.length}, Height: {self.width}, Perimeter: {self.perimeter}, Area: {self.area}")
+        print(f"Length: {self.length}, Width: {self.width}, Perimeter: {self.perimeter}, Area: {self.area}")
     
 class Parallelepipede(Rectangle):
     def __init__(self, h, *args):
@@ -43,6 +50,19 @@ class Parallelepipede(Rectangle):
     def calc_volume(self):
         self.volume = self.length * self.width * self.height
         
+    def display(self):
+        print(f"Length: {self.length}, Width: {self.width}, Height: {self.height}, Volume: {self.volume}")
+        
+print("\nRectangle")
+r = Rectangle(3, 5)
+r.calc_perimeter()
+r.calc_area()
+r.display()
+
+print("\nParallelepipede")
+p = Parallelepipede(10, 3, 5)
+p.calc_volume()
+p.display()
 
 #Person
 
@@ -61,3 +81,11 @@ class Student(Person):
         
     def display_student(self):
         print(f"Name: {self.name}, Age: {self.age}, Section: {self.section}")
+        
+print("\nPerson")
+per = Person("John", 32)
+per.display()
+
+print("\nStudent")
+s = Student("B", "Frank", 22)
+s.display_student()

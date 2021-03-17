0c4d658bc7a1444df77a3a72764ed29fcc9e141a
Sean <seanwilcox@comcast.net>
2021-03-16 18:35:20 -0700

    Updates

--- Students/Sean/python/Lab06.py
+++ Students/Sean/python/Lab06.py
@@ -5,26 +5,31 @@ lowers = string.ascii_lowercase
 uppers = string.ascii_uppercase
 numbers = string.digits
 specials = string.punctuation
+while True:
+    def pw(l,u,n,s):
+        
+    
+    try:
+        lower_choice = int(input("How many lowercase letters do you want in your password?\n>"))
+        upper_choice = int(input("How many uppercase letters do you want in your password?\n>"))
+        number_choice = int(input("How many numbers do you want in your password?\n>"))
+        special_choice = int(input("How many special characters do you want in your password?\n>"))
+    except Exception as exc:
+        print(exc)
+    for x in range(lower_choice):
+        password = random.choice(lowers)
 
-lower_choice = int(input("How many lowercase letters do you want in your password?\n>"))
-upper_choice = int(input("How many uppercase letters do you want in your password?\n>"))
-number_choice = int(input("How many numbers do you want in your password?\n>"))
-special_choice = int(input("How many special characters do you want in your password?\n>"))
+    for x in range(upper_choice):
+        password += random.choice(uppers)
 
-for x in range(lower_choice):
-    password = random.choice(lowers)
+    for x in range(number_choice):
+        password += random.choice(numbers)
 
-for x in range(upper_choice):
-    password += random.choice(uppers)
+    for x in range(special_choice):
+        password += random.choice(specials)
 
-for x in range(number_choice):
-    password += random.choice(numbers)
+    shuffling = list(password)
+    random.shuffle(shuffling)
+    shuffled = ''.join(shuffling)
 
-for x in range(special_choice):
-    password += random.choice(specials)
-
-shuffling = list(password)
-random.shuffle(shuffling)
-shuffled = ''.join(shuffling)
-
-print(shuffled)
+    print(shuffled)

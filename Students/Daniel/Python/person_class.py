e00d31a4084b9952babc75f7f9eb160d0b3a4b75
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-16 18:12:47 -0700

    Person class complete.

--- Students/Daniel/Python/person_class.py
+++ Students/Daniel/Python/person_class.py
@@ -1,7 +1,7 @@
 class Person:
     def __init__(self, name, age):
-        name = self.name
-        age = self.age
+        self.name = name
+        self.age = age
         
 
     def display(self):
@@ -9,12 +9,16 @@ class Person:
 
 class Student(Person):
     def __init__(self, name, age, section):
-        name = self.name
-        age = self.age
-        section = self.section
+        self.name = name
+        self.age = age
+        self.section = section
 
     def display(self):
         print(self.name, self.age, self.section)
 
 
-p1 = Person("Heather", 29,"HR")
+p1 = Person("Heather", 29)
+p1.display()
+
+s1 = Student("Daniel", 31, "HR")
+s1.display()

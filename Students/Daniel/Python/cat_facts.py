7af76a5b26ea8ca899c71944aa4c10ddeacd4ece
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-03-18 21:22:57 -0700

    Added REPL and .json()

--- Students/Daniel/Python/cat_facts.py
+++ Students/Daniel/Python/cat_facts.py
@@ -1,15 +1,26 @@
 import requests
-import json
 import random
 
 
-x = requests.get(f"https://cat-fact.herokuapp.com/facts")
-x = json.loads(x.text)
-response_list = []
-response_list.append(x[0]['text'])
-response_list.append(x[1]['text'])
-response_list.append(x[2]['text'])
-response_list.append(x[3]['text'])
-response_list.append(x[4]['text'])
+class RandomCatFact:
 
-print(random.choice(response_list))
+        def make_request(self):
+            x = requests.get(f"https://cat-fact.herokuapp.com/facts")
+
+            x = x.json()
+
+            response_list = []
+
+            for response in range(0,len(x),1):
+                response_list.append(x[response]['text'])
+            return random.choice(response_list)
+
+r1 = RandomCatFact()
+while True:
+    user_choice = input("Would you like to see a random cat fact? Y for Yes and N for No: ")
+    if user_choice == "Y":
+        print(r1.make_request())
+    else:
+        print("This program is not for you. ")
+        break
+

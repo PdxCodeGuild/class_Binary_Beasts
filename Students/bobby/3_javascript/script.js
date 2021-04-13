5945235877c29a5d31fb7d2bdc549be07885c320
Bobby <bobbyestes68@gmail.com>
2021-04-12 21:35:01 -0700

    loading lab #5

---
+++ Students/bobby/3_javascript/script.js
@@ -0,0 +1,78 @@
+const a = 5
+const fruits = ["apple", "rear"] // this is an array
+
+const person = {
+    age = "5",
+    name = "david"
+}
+// this is a comment
+
+/* comment */
+
+const name = prompt("enter your name") // this is an input
+
+console.log() // print
+alert() // 
+
+let a = 1
+const s = "hello world"
+console.log(`i'd like to say ${s}`)
+
+// use === to compare
+// !== not equal
+// 
+const temperature = 55
+if (temperature < 60) {
+    console.log("cold")
+}
+    else if (temperature < 80) {
+
+}
+// this is a function
+function min(a,b) {
+    return (a<b) ? a : b
+}
+console.log(min(5,4))
+
+// obljects "Everything is an object" Like a library in python
+const person = {
+    first_name : "alex",
+    last_name : "d",
+    printFullName(){
+        return `${this.first_name}`
+    }
+}
+console.log(person)
+console.log(person(printFullName))
+console.log(person.last_name)
+
+// (`hello my name is ${x}`) use the back ticks like an f string and $ sign to access variable
+
+// .push adds to the end of the array
+
+// loops
+
+// while
+
+let i = 0
+
+While (i < 10){
+    console.log(i)
+    i++
+}
+
+// for
+const fruits = ["pear", "apple", "bananna"]
+for (let i = 0 ; i<fruits.length; i++){
+    console.log(fruits(key))
+}
+// || this is or
+// && this is and
+
+
+// Functions
+function add(a, b){
+    return a + b
+}
+console.log(add)
+

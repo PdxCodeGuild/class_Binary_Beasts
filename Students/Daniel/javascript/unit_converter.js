51255bb58aba0abeb6d19d0af117c916c1e005ab
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-04-12 21:33:01 -0700

    Unit converter v2 in progress

--- Students/Daniel/javascript/unit_converter.js
+++ Students/Daniel/javascript/unit_converter.js
@@ -1,5 +1,17 @@
 const readline = require('readline-sync');
-let userInput = parseInt(readline.question('What is the distance in feet?: '));
-conv_distance = userInput * 0.3048
+const conv_dict = {
+    "ft" : 0.3048,
+    "mi" : 1609.34,
+    "m"  : 1,
+    "km" : 1000,
+};
+
+let userDistance = parseInt(readline.question('What is the distance in feet?: '));
+let userUnits = readline.question("What are the current units?: ");
+
+
+conv_distance = userDistance * conv_dict[userUnits];
 conv_distance = conv_distance.toFixed(4);
-console.log(`${userInput} ft is ${conv_distance} m`);
+console.log(`${userDistance} ${userUnits} is ${conv_distance} m`);
+
+

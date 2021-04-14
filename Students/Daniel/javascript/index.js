83278e31eb74605beed8789ea157403219bbed01
Daniel Leo Reyes <danielreyes61@gmail.com>
2021-04-13 20:59:15 -0700

    Finished unit converter

--- Students/Daniel/javascript/index.js
+++ Students/Daniel/javascript/index.js
@@ -1,4 +1,4 @@
 
-const readline = require('readline-sync');
-let userInput = parseInt(readline.question('enter a number: '));
-console.log(`you entered: ${userInput}`);
+// const readline = require('readline-sync');
+// let userInput = parseInt(readline.question('enter a number: '));
+// console.log(`you entered: ${userInput}`);
--- Students/Daniel/javascript/unit_converter.js
+++ Students/Daniel/javascript/unit_converter.js
@@ -4,14 +4,25 @@ const conv_dict = {
     "mi" : 1609.34,
     "m"  : 1,
     "km" : 1000,
+    "yd" : 0.9144,
+    "in" : 0.0254,
 };
 
-let userDistance = parseInt(readline.question('What is the distance in feet?: '));
-let userUnits = readline.question("What are the current units?: ");
+let distance = parseInt(readline.question('What is the distance?: '));
+let starting_units = readline.question("What are the starting units?: ");
+let units_to_convert_to = readline.question("What are the units to convert to?: ");
 
 
-conv_distance = userDistance * conv_dict[userUnits];
-conv_distance = conv_distance.toFixed(4);
-console.log(`${userDistance} ${userUnits} is ${conv_distance} m`);
+conv_distance = distance * conv_dict[starting_units];
+
+conv_distance = conv_distance / conv_dict[units_to_convert_to];
+
+conv_distance = conv_distance.toFixed(8);
+conv_distance_split = conv_distance.split('.');
+
+
+console.log(`${distance} ${starting_units} is ${Number(conv_distance)} ${units_to_convert_to}`);
+    
+
 
 

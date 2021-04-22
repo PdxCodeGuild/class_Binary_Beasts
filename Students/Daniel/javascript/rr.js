b28b2c7a2d8c16288d1ecb54ab96e5eb51020e19
Daniel Reyes <danielreyes61@gmail.com>
2021-04-21 19:12:58 -0700

    Random redirector complete

---
+++ Students/Daniel/javascript/rr.js
@@ -0,0 +1,5 @@
+console.log("hello");
+url_list = ["https://www.w3schools.com/js/js_arrays.asp","https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array","https://www.tutorialspoint.com/javascript/javascript_arrays_object.htm"];
+
+location = url_list[Math.floor((Math.random() * 3))]
+

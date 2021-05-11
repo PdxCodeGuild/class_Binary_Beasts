3df900cbe25b1f01151423475c9de363cc066369
Daniel Reyes <danielreyes61@gmail.com>
2021-05-10 21:08:06 -0700

    Lab 1 Done

--- Students/Daniel/Django/one/sampleproject/settings.py
+++ Students/Daniel/Django/one/sampleproject/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'sampleproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,3 +120,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]

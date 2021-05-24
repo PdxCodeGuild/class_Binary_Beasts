c8867f4bc3a2697bc78f4697294859a337a8e640
Bobby <bobbyestes68@gmail.com>
2021-05-24 16:46:37 -0700

    loading lab #4

---
+++ Students/bobby/Django/lab_4/book_app/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from .models import Author, Book, LandBook
+# Register your models here.
+
+admin.site.register(Author)
+admin.site.register(Book)
+admin.site.register(LandBook)
+
+# Register your models here.

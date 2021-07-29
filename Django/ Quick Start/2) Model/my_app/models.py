b1298cb5d74513873f82be4ed37676f8b0de93dd
alexdemichieli <demichieli.alessandro@gmail.com>
2021-07-28 20:44:09 -0700

    added extra notes

--- Django/ Quick Start/2) Model/my_app/models.py
+++ Django/ Quick Start/2) Model/my_app/models.py
@@ -5,8 +5,15 @@ class Blog(models.Model):
         title = models.CharField(max_length = 200)
         text = models.TextField(max_length = 500)
         pub_date = models.DateField()
-        user = models.ForeignKey(User, on_delete = models.CASCADE, null = True , blank = True)
 
 
         def __str__(self):
             return self.title
+
+
+class Author(models.Model):
+    blog = models.ForeignKey(Blog, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.name
